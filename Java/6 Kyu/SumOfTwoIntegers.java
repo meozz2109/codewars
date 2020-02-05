2c4fcd5dc7c863ad58647d8a59eac5a8bd4c82aa
Do Van Minh <minhstd99@gmail.com>
2020-02-05 18:41:00 +0700

    add another Codewars 's Java solution.

---
+++ Java/6 Kyu/SumOfTwoIntegers.java
@@ -0,0 +1,15 @@
+// Sum of Two Integers :
+// Given Two integers a , b , find The sum of them , BUT You are not allowed to use the operators + and -
+
+public class Solution
+{
+    public static int add(int x, int y)
+    {
+      if (y == 0) {
+      		return x;
+    	}
+    else {
+        	return add( x ^ y, (x & y) << 1);
+    	}
+    }
+}
